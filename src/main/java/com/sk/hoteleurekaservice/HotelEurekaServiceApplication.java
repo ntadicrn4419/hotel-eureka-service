44dbf9b0076a405ee09b9ad5fffa19486b2c6a28
Nikola Tadic <ntadic4419rn@raf.edu.rs>
2022-01-14 15:17:37 +0100

    Initial commit

---
+++ src/main/java/com/sk/hoteleurekaservice/HotelEurekaServiceApplication.java
@@ -0,0 +1,15 @@
+package com.sk.hoteleurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class HotelEurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelEurekaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sk/hoteleurekaservice/HotelEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.hoteleurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelEurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
